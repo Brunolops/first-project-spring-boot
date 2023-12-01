2261c36d679fc64dcbc0e161f8bd7a34dbf5fbdc
Brunolops <[200~brunooliveira99874@hotmail.com>
2023-12-01 08:07:56 -0300

    project-created

---
+++ src/main/java/com/hctDevCity/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.hctDevCity.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hctDevCity/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hctDevCity.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
